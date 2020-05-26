3c36e74b1c3dd72a638ce8c5c1dc2ce9bbb0fe06
stiglitzjauna <65974832+stiglitzjauna@users.noreply.github.com>
2020-05-26 20:40:50 +0200

    Propongo cambio de titulo

--- inicio.php
+++ inicio.php
@@ -3,7 +3,7 @@
 <html lang="es">
   <head>
     <meta charset="utf-8">
-    <title>Inicio JS14 JQUERY COMPLETO </title>
+    <title>Inicio JS14 JQUERY COMPLETO. Guille quiere un completo</title>
     <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" href="js/bootstrap.min.css">
